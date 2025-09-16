b84f211529fd527fea31e4b6525c14aec45cfd7a
Mohit-dev-dot <mohitsh0027@gmail.com>
2025-09-16 23:18:26 +0530

    Update mongodb.js

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -2,7 +2,8 @@ import mongoose from 'mongoose'
 
 const connectDB = async() => {
     mongoose.connection.on('connected', ()=>{
-        console.log('Connected to MongoDB')
+        console.log(console.log("MONGO_URI is:", process.env.MONGO_URI);
+)
     })
     await mongoose.connect(`${process.env.MONGODB_URI}/e-commerce`)
 }
